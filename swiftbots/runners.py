595fa459474d2d9a942793b20ed606aad0cfd909
ondache <211723208+ondache@users.noreply.github.com>
2025-12-02 20:24:34 +0100

    added optional timeout for oneshot runs

--- swiftbots/runners.py
+++ swiftbots/runners.py
@@ -135,15 +135,16 @@ def run_async(container: AppContainer) -> None:
     asyncio.run(start_async_loop(container))
 
 
-async def run_oneshot_async(container: AppContainer, run_with: dict) -> None:
-    assert len(container.bots) == 1, 'Only one bot is allowed to run oneshot'
-    bot = container.bots[0]
-    message = run_with
-    await bot.before_start_async()
-    entry = compose_middlewares(bot, bot._middlewares)
-    await entry(message)
-    await bot.before_close_async()
+async def run_oneshot_async(container: AppContainer, run_with: dict, timeout: float | None = None) -> None:
+    async with asyncio.timeout(timeout):
+        assert len(container.bots) == 1, 'Only one bot is allowed to run oneshot'
+        bot = container.bots[0]
+        message = run_with
+        await bot.before_start_async()
+        entry = compose_middlewares(bot, bot._middlewares)
+        await entry(message)
+        await bot.before_close_async()
 
 
-def run_oneshot(container: AppContainer, run_with: dict) -> None:
-    asyncio.run(run_oneshot_async(container, run_with))
+def run_oneshot(container: AppContainer, run_with: dict, timeout: float | None = None) -> None:
+    asyncio.run(run_oneshot_async(container, run_with, timeout))
